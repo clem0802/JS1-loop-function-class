39a2d0cab7f80f3e269dc2b2ad9c0731a5e0759f
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-10-22 10:31:35 +0200

    Create 2.81ArrowFunctions.js

---
+++ 2.81ArrowFunctions.js
@@ -0,0 +1,73 @@
+// 2021.10.21 FROM (MIMO) 
+/*----------------------------------*/
+// ES6 (JavaScript) - ARROW FUNCTIONS
+// they are used to write cleaner and shorter code
+// ARROW FUNCTIONS are a type of ANONYMOUS function => NO FUNCTION NAME
+//! if wa want to call these functions, we need to store them inside a variable 
+//! and call the variable with parentheses at the end
+// no need of the "function" keyword or the function name
+
+function getGreeting(){
+    return "Hello";
+};
+
+var greeting = () => "Hi";
+
+console.log(getGreeting()); // Hello
+console.log(greeting()); // Hi
+
+
+
+
+// 2021.10.22 FROM (MM) 
+/*----------------------------------*/
+// like with normal functions, we can add PARAMETERS inside the PARENTHESES
+// right after the parentheses and before the opening curly brace is where we add the arrow operator =>
+// we use this operator right before the code block or expression we want the function to execute
+//! just like with traditional functions, we use curly braces {} to contain an arrow function's code block
+
+function getGreeting(firstName){
+    return "Greetings!" + firstName;
+}
+
+var welcome = (firstName) => { //!
+    return "Hi, " + firstName;
+}
+
+console.log(welcome("Isaac")); // Hi, Isaac
+
+
+
+/*----------------------------------*/
+const greetings = () => {
+    const name = "Isaac";
+    return "Hello my son " + name;
+}
+console.log(greetings);
+
+
+
+const greetings = () => {
+    let position = 20;
+    return "Hi Jane";
+}
+greetings(); // Hi Jane
+
+
+
+const getVehicleCount = () => {
+    count = 4000;
+    return count;
+}
+console.log(getVehicleCount()); // 4000
+
+
+
+/*----------------------------------*/
+// remove curly braces {}
+// remove "return"
+const greetings = () => "Welcome Isaac";
+console.log(greetings()); // Welcome Isaac
+
+const getVehicleCount = () => 400;
+console.log(getVehicleCount()); // 400
