65d47e746eb71158e9afb5d6c3215f51b7cea9b9
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-10-22 08:58:47 +0200

    Create 9CounterVariable.js

---
+++ 9CounterVariable.js
@@ -0,0 +1,40 @@
+// 2021.10.12 FROM (MM) //! TO REVIEW AND ADD on 10.14 !!!!
+/*----------------------------------*/
+// COUNTER VARIABLE
+
+var counter = 1;
+while(counter<4){
+    console.log(counter);
+    counter++; // 1  2  3
+}
+
+var counter = 1;
+while(counter<10){
+    console.log(counter);
+    counter++; // 1  2  3  4  5  6  7  8  9
+}
+
+var counter = 5;
+while(counter<10){
+    counter++;
+    console.log(counter); // increment first, => 6  7  8  9
+}
+
+var listNumber = 1;
+while(listNumber<11){
+    console.log(listNumber + ". Camera YES YES");
+    listNumber++;
+}
+/* (output)
+1. Camera
+2. Camera
+3. Camera
+4. Camera
+5. Camera
+6. Camera
+7. Camera
+8. Camera
+9. Camera
+*/
+
+//! (2021.10.13)
