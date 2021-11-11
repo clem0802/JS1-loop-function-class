48d31494aeceeced80020ce47e167fc1eeaa521f
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-11 20:00:05 +0100

    Create 1.81(IF).js

---
+++ 1.81(IF).js
@@ -0,0 +1,15 @@
+//---------------------
+if (true) {
+  console.log("Welcome to Facebook");
+  console.log("Take a moment to complet your profile");
+}
+// Welcome to Facebook
+// Take a moment to complet your profile
+
+
+
+//---------------------
+var display = true;
+if (display) {
+  console.log("To display or not to display"); // To display or not to display
+}
