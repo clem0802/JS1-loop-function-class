07761af626dff10e3e0c1e848026c8ff87d1a1d1
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-11 12:15:11 +0100

    Create 1.7Types.js

---
+++ 1.7Types.js
@@ -0,0 +1,32 @@
+//-------------------------
+var result = "high score: " + true;
+console.log(result); // high score: true
+
+
+//-------------------------
+// display "1984" in the console
+console.log(19 + "84"); // 1984 (numbert + string)
+
+
+//-------------------------
+var bluetoothOn = true;
+var label = "Bluetooth on: " + true;
+console.log(label); // Bluetooth on: true (string value => here, string + boolean)
+
+
+//-------------------------
+var sugar = 100;
+console.log("Add " + sugar + " grams of sugar"); // Add 100 grams of sugar
+
+
+//-------------------------
+var movie = "Jokker";
+var score = 90;
+console.log(movie + " audience score: " + score); // Jokker audience score: 90
+
+
+//-------------------------
+var user = "Joe";
+var isOnline = true;
+var message = user + " is online: " + isOnline;
+console.log(message); // Joe is online: true
