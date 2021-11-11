5f20f6d413cd5b84d2e3752839bccbfde510b8a1
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-11 11:26:25 +0100

    Create 1.63Variable-(=,>=,<=).js

---
+++ 1.63Variable-(=,>=,<=).js
@@ -0,0 +1,26 @@
+//-----------------------
+var score = 32;
+var minScore = 30;
+var pass = score >= minScore;
+console.log(pass); // true
+
+
+//-----------------------
+// check if "temperature" is lower than 32 degrees and store the result of the comparison in a variable
+var temperature = 30; // (var provided)
+var currentTemp = temperature < 32;
+
+
+//-----------------------
+// check if "value" is greater than or equal to "minimum" and store the result of the comparison in "giveDiscount"
+var value = 90;
+var minimum = 100;
+var giveDiscount = value >= minimum;
+console.log(giveDiscount);
+
+
+//-----------------------
+// check if "emails" is greater than or equal to "capacity" and store the result of the comparison in a variable called "full"
+var capacity = 1000;
+var emails = 157;
+var full = emails >= capacity;
