f242dc1a2f6ce8034dd3456c578ef8587dcce915
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-12 20:40:05 +0100

    Update 1.81(IF).js

--- 1.81(IF).js
+++ 1.81(IF).js
@@ -13,3 +13,34 @@ var display = true;
 if (display) {
   console.log("To display or not to display"); // To display or not to display
 }
+
+
+
+//---------------------
+var account = "premium";
+if (account === "premium") {
+  console.log("Check out our new premium features.");
+}
+
+
+
+//---------------------
+// RECORD = TIME;
+var record = 9.69;
+var time = 9.58;
+
+if (time < record) {
+  record = time; //!
+  console.log("Record broken"); // Record broken
+}
+
+
+
+//---------------------
+// code a comparison with (<) to run the code bock when "batteryLevel" is less than 100
+var batteryLevel = 75;
+var doUpdate = true;
+if (batteryLevel < 100) {
+  doUpdate = false;
+  console.log("Connect your phone to power to perform update"); // Connect your phone to power to perform update
+}
