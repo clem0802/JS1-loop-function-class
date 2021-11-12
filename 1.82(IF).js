703fdd897568ec4d662b0c6add861960d351b7ce
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-12 21:24:17 +0100

    Update 1.82(IF).js

--- 1.82(IF).js
+++ 1.82(IF).js
@@ -1,9 +1,38 @@
+//----------------------------
 var subject = "Math";
 var grade = "A";
-if () {
+if (subject === "Math") {
 console.log("Give " + subject + " a chance");
-if () {
+if (grade === "A") {
 console.log(
 "Today is a grat day for solving equations!"
   );
 }
+  
+  
+  
+//----------------------------
+var missingInput = "address";
+var getAddress = missingInput === "address";
+if(getAddress) {
+  console.log("Please fill in your shipping address");
+}
+  
+  
+  
+//----------------------------
+var directFlight = true;
+var getTicket = directFlight === true;
+if(getTicket) {
+  console.log("Direct flight found");
+}
+
+
+
+var genre = "jazz";
+var artist = "John Coltrane";
+var recommendArtist = genre === "jazz";
+if(recommendArtist) {
+  console.loge("New recommendation: " + artist);
+}
+
