6627e7db1de12ea062cc8375de8e2fd2f606aa29
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-13 17:32:38 +0100

    Create 1.97Loops(while-counter).js

---
+++ 1.97Loops(while-counter).js
@@ -0,0 +1,25 @@
+/*---------------------------*/
+var number = -5;
+while (number < 5) {
+  console.log(number);
+  number++;
+}
+// -5  -4  -3  -2  -1  0  1  2  3  4  
+
+
+
+/*---------------------------*/
+var speed = 0;
+while (speed < 100) {
+  speed += 10;
+}
+console.log(speed); // 100
+
+
+
+/*---------------------------*/
+var speed = 20;
+while (speed < 100) {
+  speed += 10;
+}
+console.log(speed); // 100
