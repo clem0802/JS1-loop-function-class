44aa006271f2bbcc50f9d9d044bc7ac4abd4bb1f
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-13 20:24:46 +0100

    Update 1.97Loops(FOR-counter).js

--- 1.97Loops(FOR-counter).js
+++ 1.97Loops(FOR-counter).js
@@ -1,3 +1,10 @@
+/*---------------------------*/ "FOR" LOOP
+/*---------------------------*/
+// FOR loop is easier to read than WHILE loop
+// the number of times a FOR loop repeats is easy to find inside the parentheses
+// 'cause the "FOR" loop prevents us from writing infinite loops
+
+
 /*---------------------------*/
 console.log("Best Cars of 1885:");
 for (var i = 1; i <= 3; i++) {
@@ -30,3 +37,90 @@ Fastest runners:
 2.
 3.
 */
+
+
+
+/*---------------------------*/
+for (var i=0; i<3; i++) {
+console.log("Poverty should be banned!");
+}
+// Poverty should be banned!
+// Poverty should be banned!
+// Poverty should be banned!
+
+
+
+/*---------------------------*/
+console.log("#");
+var line = "";
+for (var i=1; i<=5; i++) {
+  line += "~";
+  console.log(line)
+}
+// *
+// ~
+// ~~
+// ~~~
+// ~~~~
+// ~~~~~
+
+
+
+/*---------------------------*/
+var lyric = "I want to hold you h";
+for (var i=0; i<6; i++) {
+  lyric += "a";
+}
+lyric += "nd";
+console.log(lyric); // I want to hold you haaaaaand
+
+
+
+/*---------------------------*/
+var notifictaions = 4;
+for (var i=0; i<notifications; i++) {
+  console.log("You've got mail!");
+}
+// You've got mail!
+// You've got mail!
+// You've got mail!
+// You've got mail!
+
+
+
+/*---------------------------*/ (VISAGE D'UN BONHOMME)
+console.log("_ _");
+console.log(". .");
+var nose = "  >";
+for (var i=0; i<3; i++) {
+  nose += "-"
+}
+console.log(nose);
+console.log(" o ");
+/*
+_ _
+. .
+  >---
+ o
+*/
+
+
+
+var timesToRepeat = 3;
+var message = "New comment!";
+var counter = 0;
+while (counter < timesToRepeat) {
+  console.log(message);
+  counter++;
+  console.log("Monday, 11 am");
+}
+// New comment!
+// Monday, 11 am
+// New comment!
+// Monday, 11 am
+// New comment!
+// Monday, 11 am
+
+
+
+
