9ba72c32cf2136a8d7f27c3bdc64d0b481a654b3
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-13 19:05:36 +0100

    Update and rename 1.97Loops(while-counter).js to 1.97Loops(for-counter).js

---
+++ 1.97Loops(for-counter).js
@@ -0,0 +1,32 @@
+/*---------------------------*/
+console.log("Best Cars of 1885:");
+for (var i = 1; i <= 3; i++) {
+  console.log(i + ".");
+}
+/*
+Best Cars of 1885:
+1.
+2.
+3.
+*/
+
+
+
+/*---------------------------*/
+for (var i = -5; i < 0; i++) {
+  console.log(i);
+} //  -5  -4  -3  -2  -1
+
+
+
+/*---------------------------*/
+console.log("Fastest runners:");
+for (var index = 1; index < 4; index++) {
+  console.log(index + ".");
+}
+/*
+Fastest runners:
+1.
+2.
+3.
+*/
