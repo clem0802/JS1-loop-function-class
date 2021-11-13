b79515c5a7262dbc6fc26233fb6531aa92f07840
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-13 13:04:01 +0100

    Update 1.65SelfAssignVariable.js

--- 1.65SelfAssignVariable.js
+++ 1.65SelfAssignVariable.js
@@ -1,4 +1,52 @@
 //------------------------- (SELF-ASSIGNING VARIABLES)
+//-------------------------
 var wallet = 250;
 wallet -= 50
 console.log(wallet); // 200
+
+
+
+//-------------------------
+var languages = "HTML CSS3;
+languages += " JavaScript";
+console.log(languages); // HTML CSS JavaScript
+
+
+
+//-------------------------
+var welcome = "Welcome";
+welcome += " Isaac";
+console.log(welcome); // Welcome, Isaac
+
+
+
+//-------------------------
+var temperature = 0;
+temperature -= 7;
+console.log(temperature); // -7
+
+
+
+//------------------------- (INCREMENT OPERATOR: ADD 1) //!
+//-------------------------
+var tries = 2;
+tries++;
+console.log(tries); // 3 =>  2+1=3
+
+
+
+//------------------------- (DECREMENT OPERATOR: MINUS 1) //!
+var hoursLeft = 30;
+hoursLeft--;
+console.log(hoursLeft); // 29 => 30-1=29
+
+
+
+//-------------------------
+var sentence = "I think, ";
+sentence += "therefore ";
+sentence += "I code.";
+console.log(sentence); // I think, therefore I code.
+
+
+
