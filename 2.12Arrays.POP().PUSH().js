3ef9f601e4e81c1a60210f4c70d2d13d034c6931
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-13 21:58:45 +0100

    Update and rename 2.12Arrays(.POP).js to 2.12Arrays.POP().PUSH().js

---
+++ 2.12Arrays.POP().PUSH().js
@@ -0,0 +1,30 @@
+/*------------------------*/  ARRAYS
+/*------------------------*/
+
+
+/*------------------------*/ .POP()  => REMOVE the last element of an array
+var doorCode = [2, 5, 0, 0, 1];
+var digit = doorCode.pop();
+console.log(digit); // 1
+
+
+
+/*------------------------*/ .POP()  => REMOVE 
+var characters = ["mario", "tintin", "princess peach"];
+var player1 = characters.pop();
+console.log("Player 1: " + player1); // Player 1: princess peach
+
+
+
+
+//---------------------------------------------------------------------
+/*------------------------*/ .PUSH()  => ADD a new item to the end of an array
+var numbers = [2, 5, 0, 0, 1];
+numbers.push(3);
+console.log(numbers); // [1, 2, 3]
+
+
+/*------------------------*/ .PUSH()  => ADD 
+var forecast = ["sunny", "cloudy", "snowy"];
+forecast.push("windy");
+console.log(forecast); // ["sunny", "cloudy", "snowy", "windy"]
