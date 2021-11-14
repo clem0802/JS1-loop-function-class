94927aa85f45d6b486fa47f919c3bae62d89c07d
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-14 15:00:14 +0100

    Create 2.23Function(PARAMETER).js

---
+++ 2.23Function(PARAMETER).js
@@ -0,0 +1,24 @@
+// 2021.11.14 FROM (MM) 
+/*----------------------------------*/
+// FUNCTIONS with (PARAMETER)
+// (parameters) are there to PASS SPECIFIC INFORMATIONS to FUNCTIONS
+
+
+//------------------------
+function makeGerund(word) {
+  var result = word + "ing";
+  console.log(result);
+}
+makeGerund("Jump"); // Jumping
+
+
+//------------------------
+function displayTriple(number) {
+  var triple = number * 3;
+  console.log(triple);
+}
+displayTriple(100000); // 300000
+
+
+
+
