59ebaad344f8d690bdce4bf293c8aec4356ad313
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-14 16:39:05 +0100

    Create 2.24Function(ParameterS).js

---
+++ 2.24Function(ParameterS).js
@@ -0,0 +1,22 @@
+// 2021.11.14 FROM (MM) 
+/*----------------------------------*/
+// FUNCTIONS with (PARAMETER"S")
+
+
+//------------------------
+function showWinners(first, second) {
+  console.log("1st: " + first);
+  console.log("2nd: " + second);
+}
+showWinners("Helen", "Joe");
+// 1st: Helen
+// 2nd: Joe
+
+
+//------------------------
+function mashWords(first, second, third) {
+  return first + second + third;
+}
+var result = mashWords("big", "bad", "wolf");
+console.log(result);
+// bigbadwolf
