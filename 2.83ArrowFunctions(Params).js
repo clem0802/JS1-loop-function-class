a7125743d8557820ea013327d86dd809535946d6
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-14 22:34:13 +0100

    Create 2.83ArrowFunctions(Params).js

---
+++ 2.83ArrowFunctions(Params).js
@@ -0,0 +1,9 @@
+// 2021.10.22 FROM (MM) 
+/*----------------------------------*/
+// ES6 (JavaScript) - ARROW FUNCTIONS => (PARAMETERS)
+
+
+// what is wrong with the code below?
+// we need to set a default value to avoid displaying "NaN"
+const getSquare = (number) => number * 2;
+console.log(getSquare()); // NaN
