a7ed0015e96c816808b37a3b98fc0e43c1172630
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-14 22:31:05 +0100

    Update 2.82ArrowFunctions(parameters).js

--- 2.82ArrowFunctions(parameters).js
+++ 2.82ArrowFunctions(parameters).js
@@ -17,18 +17,18 @@ const getPrice = (total) => {
     return total - (discount * total);
 }
 console.log(getPrice(500)); // 450
-
+//--------
 
 const getPrice = total => {  //! only 1 parameter, so () omitted is OK
     const discount = 0.10;
     return total - (discount * total);
 }
 console.log(getPrice(500)); // 450
-
+//--------
 
 const getPrice = (discount) => 500 - (discount * 500);
 console.log(getPrice(0.10)); // 450
-
+//--------
 
 const getPrice = discount => 100 - discount;
 console.log(getPrice(10)); // 90
@@ -42,14 +42,14 @@ const getPrice = (total) => {
     return total + (tax * total);
 }
 console.log(getPrice()); // NaN
-
+//--------
 
 const getPrice = (total) => { //! set the default value in console.log
     const tax = 0.16;
     return total + (tax * total);
 }
 console.log(getPrice(200)); // 232  
-
+//--------
 
 const getPrice = (total=200) => { //! or set the default value inside the parentheses
     const tax = 0.16;
@@ -78,7 +78,7 @@ const getPrice = (total) => {
     return total - (discount * total);
 }
 console.log(getPrice(1000)); // 900
-
+//--------
 
 const getPrice = (total=1000, discount=10) => total - (discount / 100 * total);
 console.log(getPrice()); // 900
@@ -89,19 +89,19 @@ console.log(getPrice()); // 900
 /*----------------------------------*/
 const getSquare = (number, multiplier) => number * multiplier;
 console.log(getSquare(4,5)); // 20
-
+//--------
 
 const getRatio = (nom, denom) => {
     return nom / denom;
 }
 console.log(getRatio(20,2)); // 10
-
+//--------
 
 const getRatio = (nom, denom = 2) => {
     return nom / denom;
 }
 console.log(getRatio(40)); // 20
-
+//--------
 
 const getRatio = (nom, denom = 2) => {
     return nom / denom;
@@ -116,20 +116,19 @@ const getTitle = (movie) => { //! check
     return "20th Century Movie: " + movie;
 }
 console.log(getTitle("Coming Home")); // 20th Century Movie: Coming Home
-
-
+//--------
 
 const getMultiple = (number) => number * 2; 
 console.log(getMultiple(4)); // 8
-
+//--------
 
 const getSquare = (number = 0) => number * number; //! BE CAREFUL
 console.log(getSquare(5)); // 25
-
+//--------
 
 const getArea = (length, width = 3) => length * width;  //  ATTENTION, in console.log  5=length, by default
 console.log(getArea(5)); // 15
-
+//--------
 
 const getArea = (length, width = 3) => { //  ATTENTION, in console.log  10=length, by default
     return length * width;
