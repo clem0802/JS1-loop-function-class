ed7c60134a9b9e406703685e9a9da77eb8ee5cc9
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-15 16:08:35 +0100

    Create 3.5ClassInstances.js

---
+++ 3.5ClassInstances.js
@@ -0,0 +1,11 @@
+// 2021.11.15 FROM (MM) 
+/*----------------------------------*/
+// CLASSES //! INSTANCES [ created by "constructor()" ]
+
+
+/*----------------------------------*/
+class Employee {
+  constructor(compagny) {
+    this.company = company;
+  }
+}
